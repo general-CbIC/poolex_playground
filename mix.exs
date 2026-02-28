20a4dfcce377a449b3c4f1620335684f44396912
Aleksandr Sysoev <general@cbic-dev.com>
2026-02-28 14:58:21 +0300

    feat: add poolex ~> 1.4 dependency

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule PoolexExample.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:poolex, "~> 1.4"}
     ]
   end
 
