4669d3e8891e354c0ca79662ad85f6d2164c13ce
Aleksandr Sysoev <sanchous.ok@gmail.com>
2026-03-03 19:09:17 +0300

    Test acquire and release functions

--- lib/poolex_example_web/live/pool_live.ex
+++ lib/poolex_example_web/live/pool_live.ex
@@ -1,20 +1,23 @@
 defmodule PoolexExampleWeb.PoolLive do
   use PoolexExampleWeb, :live_view
 
+  @pool_id :demo_pool
+
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket), do: schedule_tick()
 
     poolex_version = Application.spec(:poolex, :vsn) |> to_string()
-    debug_info = Poolex.Private.DebugInfo.get_debug_info(:demo_pool)
+    debug_info = Poolex.Private.DebugInfo.get_debug_info(@pool_id)
     {milliseconds_from_start, _since_last_call} = :erlang.statistics(:wall_clock)
 
     {:ok,
      assign(socket,
+       acquired_workers: [],
        debug_info: debug_info,
        milliseconds_from_start: milliseconds_from_start,
        occupy_duration: 10,
-       page_title: "Poolex Dashboard",
+       page_title: "Poolex Playground",
        poolex_version: poolex_version
      )}
   end
@@ -23,7 +26,7 @@ defmodule PoolexExampleWeb.PoolLive do
   def handle_info(:tick, socket) do
     schedule_tick()
 
-    debug_info = Poolex.Private.DebugInfo.get_debug_info(:demo_pool)
+    debug_info = Poolex.Private.DebugInfo.get_debug_info(@pool_id)
     {milliseconds_from_start, _since_last_call} = :erlang.statistics(:wall_clock)
 
     {:noreply,
@@ -32,14 +35,14 @@ defmodule PoolexExampleWeb.PoolLive do
 
   @impl true
   def handle_event("add_worker", _params, socket) do
-    Poolex.add_idle_workers!(:demo_pool, 1)
-    {:noreply, assign(socket, debug_info: Poolex.Private.DebugInfo.get_debug_info(:demo_pool))}
+    Poolex.add_idle_workers!(@pool_id, 1)
+    {:noreply, assign(socket, debug_info: Poolex.Private.DebugInfo.get_debug_info(@pool_id))}
   end
 
   @impl true
   def handle_event("remove_worker", _params, socket) do
-    Poolex.remove_idle_workers!(:demo_pool, 1)
-    {:noreply, assign(socket, debug_info: Poolex.Private.DebugInfo.get_debug_info(:demo_pool))}
+    Poolex.remove_idle_workers!(@pool_id, 1)
+    {:noreply, assign(socket, debug_info: Poolex.Private.DebugInfo.get_debug_info(@pool_id))}
   end
 
   @impl true
@@ -49,7 +52,7 @@ defmodule PoolexExampleWeb.PoolLive do
         ms = n * 1_000
 
         Task.start(fn ->
-          Poolex.run(:demo_pool, fn worker ->
+          Poolex.run(@pool_id, fn worker ->
             GenServer.call(worker, {:sleep, ms}, ms + 5_000)
           end)
         end)
@@ -57,6 +60,32 @@ defmodule PoolexExampleWeb.PoolLive do
         {:noreply, socket}
 
       _ ->
+        {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_event("acquire", _params, socket) do
+    case Poolex.acquire(@pool_id) do
+      {:ok, worker} ->
+        acquired_workers = [worker | socket.assigns.acquired_workers]
+
+        {:noreply, assign(socket, acquired_workers: acquired_workers)}
+
+      {:error, _reason} ->
+        {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_event("release", _params, socket) do
+    case socket.assigns.acquired_workers do
+      [worker | acquired_workers] ->
+        Poolex.release(@pool_id, worker)
+
+        {:noreply, assign(socket, acquired_workers: acquired_workers)}
+
+      [] ->
         {:noreply, socket}
     end
   end
